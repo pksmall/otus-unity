a9bbcbcddaebf13a3d459063c723f19cc1d52114
Pavel Korzhenko <pksmall@live.ru>
2020-04-09 20:09:16 +0300

    complete hw03

---
+++ lesson03/Assets/Scripts/UI/PauseMenuLogic.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenuLogic : MonoBehaviour
+{
+    public GameObject visualBattlePart;
+    public GameObject visualPausePart;
+    public float fakeLoadTime = 0f;
+
+    public Button cancelButton;
+    public Button loadAgainButton;
+    public Button exitButton;
+
+
+    private void Awake()
+    {
+        cancelButton.onClick.AddListener(OnCancelButtonClick);
+        loadAgainButton.onClick.AddListener(OnLoadAgainButtonClick);
+        exitButton.onClick.AddListener(OnExitButtonClick);
+    }
+
+    public void OnPauseButtonClick()
+    {
+        Debug.Log("Awake: " + SceneManager.GetActiveScene().name);
+
+        visualPausePart.SetActive(true);
+        visualBattlePart.SetActive(false);
+    }
+
+    private void OnCancelButtonClick()
+    {
+        visualPausePart.SetActive(false);
+        visualBattlePart.SetActive(true);
+    }
+    private void OnLoadAgainButtonClick()
+    {
+        string name = SceneManager.GetActiveScene().name;
+        LoadScene(name);
+    }
+
+    private void OnExitButtonClick()
+    {
+        LoadScene(0);
+    }
+
+    public void LoadScene(int sceneNumber)
+    {
+        StartCoroutine(LoadGameSceneCor(sceneNumber));
+    }
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(LoadGameSceneCor(sceneName));
+    }
+
+    private IEnumerator LoadGameSceneCor(string sceneName)
+    {
+        AsyncOperation asyncLoaing = SceneManager.LoadSceneAsync(sceneName);
+        asyncLoaing.allowSceneActivation = false;
+
+        float timer = 0f;
+
+        while (timer < fakeLoadTime || asyncLoaing.progress < 0.9f)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        asyncLoaing.allowSceneActivation = true;
+        while (asyncLoaing.progress < 0.99999f)
+        {
+            yield return null;
+        }
+    }
+
+    private IEnumerator LoadGameSceneCor(int sceneNumber)
+    {
+        AsyncOperation asyncLoaing = SceneManager.LoadSceneAsync(sceneNumber);
+        asyncLoaing.allowSceneActivation = false;
+
+        float timer = 0f;
+
+        while (timer < fakeLoadTime || asyncLoaing.progress < 0.9f)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        asyncLoaing.allowSceneActivation = true;
+        while (asyncLoaing.progress < 0.99999f)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ lesson03/Assets/Scripts/UI/StartMenuPage.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+ 
+public class StartMenuPage : MonoBehaviour
+{
+    private const string BATTLE_SCENE_NAME = "Level#1";
+    public string level1 = BATTLE_SCENE_NAME;
+    public string level2 = "Level#2";
+    public Button playButton;
+    public Button exitButton;
+    public LoadingLogic loadingLogic;
+    public Button level1Button;
+    public Button level2Button;
+    public GameObject visualChoosePart;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(PlayGame);
+        exitButton.onClick.AddListener(ExitGame);
+
+        // choose level
+        level1Button.onClick.AddListener(Level1Game);
+        level2Button.onClick.AddListener(Level2Game);
+    }
+
+    private void Level1Game()
+    {
+        loadingLogic.LoadScene(level1);
+    }
+
+    private void Level2Game()
+    {
+        loadingLogic.LoadScene(level2);
+    }
+
+    private void PlayGame()
+    {
+        visualChoosePart.SetActive(true);
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
